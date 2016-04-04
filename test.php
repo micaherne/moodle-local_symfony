94829e394e4b0ba4fe6d9e81fdc8379b9bacdcdb
Michael Aherne <michael.aherne@strath.ac.uk>
2016-04-04 14:17:29 +0100

    Use mustache for generation

--- test.php
+++ test.php
@@ -1,7 +1,5 @@
 <?php
 use Symfony\Component\Validator\Validation;
-use Symfony\Component\Validator\ConstraintValidator;
-use Symfony\Component\Validator\Constraint;
 
 require_once '../../config.php';
 require_once 'vendor/autoload.php';
@@ -9,6 +7,7 @@ require_once 'vendor/autoload.php';
 class TestClass {
 
     public $url = 'http://localhost';
+    public $username = 'fjksdl @@@ # ';
 
 }
 
